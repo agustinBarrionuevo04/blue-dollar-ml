51aa6436de21fb74cd59a02aef764297089ae141
Agustin <brian.barrionuevo@mi.unc.edu.ar>
2025-12-19 18:45:44 -0300

    add more context for the train in the model

--- src/models/baselines.py
+++ src/models/baselines.py
@@ -13,3 +13,22 @@ class HowYesterday(BaselineModels):
     def predict_same_as_yesterday(self, x):
         return (x['lag_1'] > 0).astype(int).values
     
+
+
+
+def get_rsi(data, periodo=14):
+    delta = data['valor'].diff()
+    delta = delta.dropna()
+
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.ewn(com=periodo-1, adjust=False).mean()
+    avg_loss = loss.ewn(com=periodo-1, adjust=False).mean()
+
+    rs = avg_gain / avg_loss
+
+    rsi = 100 - (100 / (1 + rs))
+
+    return rsi
+
